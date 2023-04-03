18d6e48a11d4a495f106e32a4a1f041907d41708
Aiperiwa <periwa003@gmail.com>
2023-04-03 13:11:09 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,97 @@
+const pics = [
+    'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1020&q=80',
+    'https://images.unsplash.com/photo-1647891941746-fe1d53ddc7a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    'https://images.unsplash.com/photo-1581044777550-4cfa60707c03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80',
+    'https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80',
+    'https://images.unsplash.com/photo-1509631179647-0177331693ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80',
+    'https://images.unsplash.com/photo-1627483262112-039e9a0a0f16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    'https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    
+  ]
+
+  const boxImg = document.querySelector('.box-img'); // Находим div с классом "box-img"
+
+  for (let i = 0; i < pics.length; i++) {
+    const imgWrapper = document.createElement('div')
+    imgWrapper.classList.add('imgWrapper')
+    const img = document.createElement('img'); // Создаем элемент "img"
+    img.classList.add('img-box'); // Добавляем ему класс "img-box"
+    img.setAttribute('src', pics[i]); // Устанавливаем ему src атрибут с url-адресом из массива pics
+    imgWrapper.appendChild(img);
+    boxImg.appendChild(imgWrapper);
+}
+
+
+const imgBoxes = document.querySelectorAll('.img-box'); // Находим все элементы с классом "img-box"
+
+for (let i = 0; i < imgBoxes.length; i++) {
+  imgBoxes[i].addEventListener('click', function() {
+    const modal = document.createElement('div'); // Создаем элемент "div" для модального окна
+    modal.classList.add('modal'); // Добавляем ему класс "modal"
+    
+    const modalImg = document.createElement('img'); // Создаем элемент "img" для увеличенной картинки
+    modalImg.setAttribute('src', this.getAttribute('src')); // Устанавливаем ему src атрибут со значением src атрибута кликнутой картинки
+    modalImg.classList.add('modal-img');
+
+
+    
+    const closeBtn = document.createElement('button'); // Создаем кнопку "close"
+    closeBtn.classList.add('close-btn'); // Добавляем ей класс "close-btn"
+    closeBtn.textContent = 'close'; // Устанавливаем ей текст "close"
+    closeBtn.addEventListener('click', function() {
+      modal.remove(); // При клике на кнопку "close" удаляем модальное окно из DOM
+   
+    })
+    modal.onclick = function() {
+         modal.remove()
+       } 
+    
+    modal.appendChild(closeBtn);  // Добавляем кнопку "close" в модальное окно
+    modal.appendChild(modalImg); // Добавляем увеличенную картинку в модальное окно
+
+    document.body.appendChild(modal); // Добавляем модальное окно в конец тела документа
+  });
+  
+}
+
+
+const button = document.querySelector('.contactMe')
+const blueContact = document.querySelector('.blueContact')
+const closeXX = document.querySelector('.closeXX')
+const contact = document.querySelector('.contact')
+const form = document.querySelector('form')
+const btnMessage = document.querySelector('.message')
+
+
+button.addEventListener('click', () => {
+    blueContact.style.display = 'flex'
+    contact.style.display = 'block'
+})
+
+closeXX.addEventListener('click', function() {
+    blueContact.style.display = 'none'
+    contact.style.display = 'none'
+})
+
+blueContact.onclick = function() {
+        blueContact.style.display = 'none'
+        contact.style.display = 'none'
+
+}
+
+btnMessage.addEventListener('click', (e) => {
+    e.preventDefault()
+    const name = form.elements.name.value
+    const email = form.elements.email.value
+    const text = form.elements.text.value
+
+    console.log(`Name: ${name}`)
+    console.log(`Email: ${email}`)
+    console.log(`Message: ${text}`)
+    form.reset()
+
+})
+
+    
+
+
